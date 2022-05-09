89f00ef7be5b569167cb623b5ad9305803564909
Joseph Kavanagh <Joseph@kav.io>
2022-05-09 14:18:04 +0100

    build: use latest `golanci-lint`

--- .pre-commit-config.requirements.sh
+++ .pre-commit-config.requirements.sh
@@ -7,4 +7,4 @@ go install github.com/securego/gosec/v2/cmd/gosec@latest
 # go-structslop
 go install github.com/orijtech/structslop/cmd/structslop@latest
 # golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.45.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
