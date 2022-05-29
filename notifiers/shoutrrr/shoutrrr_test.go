a2471b02a0db1ffe0b1e086f51c4d50656b8bff6
Joseph Kavanagh <Joseph@kav.io>
2022-05-29 16:39:15 +0100

    fix(saving): empty `service.notify.name`s were removed (#76)
    e.g. `service.notify.something` was removed if it got all its params from `notify.something`

--- notifiers/shoutrrr/shoutrrr_test.go
+++ notifiers/shoutrrr/shoutrrr_test.go
@@ -23,21 +23,22 @@ import (
 func TestShoutrrr(t *testing.T) {
 	// Test one Params
 	defaultShoutrr := Shoutrrr{
-		Options:   &map[string]string{},
-		URLFields: &map[string]string{},
-		Params:    &shoutrrr_types.Params{},
+		Options:   map[string]string{},
+		URLFields: map[string]string{},
+		Params:    shoutrrr_types.Params{},
 	}
 	test := Shoutrrr{
 		Main:         &defaultShoutrr,
 		Defaults:     &defaultShoutrr,
 		HardDefaults: &defaultShoutrr,
-		Params: &shoutrrr_types.Params{
-			"botname": "Test",
+		Params: shoutrrr_types.Params{
+			"BotName": "Test",
 		}}
 	wantedParams := map[string]string{
 		"botname": "Test",
 	}
 
+	test.initParams()
 	gotParams := test.GetParams()
 	for key := range *gotParams {
 		if (*gotParams)[key] != wantedParams[key] {
@@ -55,14 +56,15 @@ func TestShoutrrr(t *testing.T) {
 		Main:         &defaultShoutrr,
 		Defaults:     &defaultShoutrr,
 		HardDefaults: &defaultShoutrr,
-		Params: &shoutrrr_types.Params{
-			"botname": "OtherTest",
-			"icon":    "github",
+		Params: shoutrrr_types.Params{
+			"BotName": "OtherTest",
+			"Icon":    "github",
 		}}
 	wantedParams = map[string]string{
 		"botname": "OtherTest",
 		"icon":    "github",
 	}
+	test.initParams()
 	gotParams = test.GetParams()
 	for key := range *gotParams {
 		if (*gotParams)[key] != wantedParams[key] {
@@ -78,18 +80,19 @@ func TestShoutrrr(t *testing.T) {
 	// Test GetURL with one Params
 	testType := "discord"
 	testURLFields := map[string]string{
-		"token":     "bar",
-		"webhookid": "foo",
+		"Token":     "bar",
+		"WebhookID": "foo",
 	}
 	test = Shoutrrr{
 		Main:         &defaultShoutrr,
 		Defaults:     &defaultShoutrr,
 		HardDefaults: &defaultShoutrr,
 		Type:         testType,
-		URLFields:    &testURLFields,
+		URLFields:    testURLFields,
 		Params:       test.Params,
 	}
 	wantedURL := "discord://bar@foo"
+	test.initURLFields()
 	gotURL := test.GetURL()
 	if gotURL != wantedURL {
 		t.Fatalf(`Shoutrrr, GetURL - Got %v, want match for %q`, gotURL, wantedURL)
@@ -98,24 +101,65 @@ func TestShoutrrr(t *testing.T) {
 	// Test GetURL with multiple Params
 	testType = "teams"
 	testURLFields = map[string]string{
-		"group":      "something",
-		"tenant":     "foo",
-		"altid":      "bar",
-		"groupowner": "fez",
+		"Group":      "something",
+		"Tenant":     "foo",
+		"AltID":      "bar",
+		"GroupOwner": "fez",
 	}
-	testParams := &shoutrrr_types.Params{
-		"host":  "mockhost",
-		"title": "test",
+	testParams := shoutrrr_types.Params{
+		"Host":  "mockhost",
+		"Title": "test",
 	}
 	test = Shoutrrr{
 		Main:         &defaultShoutrr,
 		Defaults:     &defaultShoutrr,
 		HardDefaults: &defaultShoutrr,
 		Type:         testType,
-		URLFields:    &testURLFields,
+		URLFields:    testURLFields,
 		Params:       testParams,
 	}
 	wantedURL = "teams://something@foo/bar/fez?host=mockhost"
+	test.initParams()
+	test.initURLFields()
+	gotURL = test.GetURL()
+	if gotURL != wantedURL {
+		t.Fatalf(`Shoutrrr, GetURL - Got %v, want match for %q`, gotURL, wantedURL)
+	}
+
+	// Test Defaults
+	testType = "teams"
+	testServiceURLFields := map[string]string{
+		"Group": "something",
+	}
+	testMainURLFields := map[string]string{
+		"Group":  "main",
+		"Tenant": "foo",
+	}
+	testDefaultsURLFields := map[string]string{
+		"Group":  "default",
+		"Tenant": "default",
+		"AltID":  "bar",
+	}
+	testHardDefaultsURLFields := map[string]string{
+		"Group":      "hardDefault",
+		"Tenant":     "hardDefault",
+		"AltID":      "hardDefault",
+		"GroupOwner": "fez",
+	}
+	test = Shoutrrr{
+		Main:         &Shoutrrr{URLFields: testMainURLFields},
+		Defaults:     &Shoutrrr{URLFields: testDefaultsURLFields},
+		HardDefaults: &Shoutrrr{URLFields: testHardDefaultsURLFields},
+		Type:         testType,
+		URLFields:    testServiceURLFields,
+		Params:       testParams,
+	}
+	wantedURL = "teams://something@foo/bar/fez?host=mockhost"
+	test.initParams()
+	test.initURLFields()
+	test.Main.initURLFields()
+	test.Defaults.initURLFields()
+	test.HardDefaults.initURLFields()
 	gotURL = test.GetURL()
 	if gotURL != wantedURL {
 		t.Fatalf(`Shoutrrr, GetURL - Got %v, want match for %q`, gotURL, wantedURL)
