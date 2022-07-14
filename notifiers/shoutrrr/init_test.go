66a19fc087b1c4cf8b5659d5ee2d46b86be8d9ba
Joseph Kavanagh <Joseph@kav.io>
2022-07-15 00:19:29 +0100

    fix(notify): crashed with notify in service, but none in notify global
    having a `service.notify.x` caused a crash if there were no `notify.*`

--- notifiers/shoutrrr/init_test.go
+++ notifiers/shoutrrr/init_test.go
@@ -383,6 +383,23 @@ func TestSliceInitWithNilElement(t *testing.T) {
 	}
 }
 
+func TestSliceInitWithDefaultMain(t *testing.T) {
+	// GIVEN a Slice and a Slice with a Main for "1"
+	slice := testSlice()
+	serviceID := "test"
+
+	// WHEN Init is called on it with a main for "1"
+	slice.Init(nil, &serviceID, &Slice{}, &Slice{}, &Slice{})
+
+	// THEN "1" has an empty main
+	if slice["1"].Main.Type != "" ||
+		len(slice["1"].Main.Options) != 0 ||
+		len(slice["1"].Main.URLFields) != 0 ||
+		len(slice["1"].Main.Params) != 0 {
+		t.Errorf("Slice['1'] was given %v, not an empty Main (%v)", slice["1"].Main, Shoutrrr{})
+	}
+}
+
 func TestSliceInitWithNilMains(t *testing.T) {
 	// GIVEN a Slice with nil Shoutrrr mains
 	slice := testSlice()
