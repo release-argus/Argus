cc64c8834fb6e5a35970b223362ce917e7350792
Joseph Kavanagh <Joseph@kav.io>
2024-03-25 20:51:30 +0000

    build(docker): switch to alpine distro
    * (CGO_ENABLED=0 + 'netgo' build tag)
    * binaries also compatible with alpine with this

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+set -e
 
 ARGUS_UID=${ARGUS_UID:-911}
 ARGUS_GID=${ARGUS_GID:-911}
 
 if [ "$(id -u)" -eq 0 ]; then
   echo "Switching user UID:GID to ${ARGUS_UID}:${ARGUS_GID}"
-  groupmod -o -g "$ARGUS_GID" argus >/dev/null
-  usermod -o -u "$ARGUS_UID" argus >/dev/null
+  sed -i -e "s/^\(argus:[^:]\):[0-9]*:[0-9]*:/\1:${ARGUS_UID}:${ARGUS_GID}:/" /etc/passwd
+  sed -i -e "s/^argus:x:[0-9]\+:argus$/argus:x:${ARGUS_GID}:argus:/" /etc/group
 
   if [ -f "/etc/argus/config.yml" ]; then
     echo "---------------------------------------------------------------------"
@@ -22,10 +23,15 @@ if [ "$(id -u)" -eq 0 ]; then
 
   echo "Applying perms to config.yml and argus.db"
   touch /app/data/argus.db
-  chown argus:argus /app/data/argus.db
+  chown ${ARGUS_UID}:${ARGUS_GID} \
+    /app \
+    /app/data \
+    /app/data/argus.db \
+    /app/config.yml
 
-  su - argus
+  echo "Starting..."
+  su-exec argus /usr/bin/argus "$@"
 fi
 
 echo "Starting..."
-argus "$@"
+exec argus "$@"
